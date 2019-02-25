e3a70866e7d55eaf08dc725039647b7884cd2a49
eraasch123 <33470494+eraasch123@users.noreply.github.com>
2019-02-24 23:45:12 -0600

    Update HW1.py

--- HW1.py
+++ HW1.py
@@ -1,18 +1,23 @@
 import pandas as pd
-import re
+    divorce = pd.read_excel('divorce.xlsx',
+                            skiprows=5,
+                            header=[0],  # skipping the header
+                            skipfooter=4,  # cleaning the footer
+                            # na_values='---', #null values
+                            usecols=22,
+                            # all the columns that we need from the dirty excel file
+                            index_col=[0])
 
-
-# read in the data to pandas dataframe ########################################################################
-rates = pd.read_excel('Part 1.2 HW1.xlsx',
-                       skiprows=2,                      # skip 1st 5 rows of excel file
-                       header=[2],                   # header is now in the first and 2nd r                   # skip last 13 rows of excel file
-                       index_col=0)             # index columns are 1st - 3rd in excel file but add total to
-                                                                # keep it out of multiindex
-
-
-new_DF = pd.melt(rates, id_vars=["State"], value_vars=['2017','2016','2015','2014','2013','2012','2011','2010','2009','2008','2007'
-                                                        ,'2006','2005','2004','2003','2002','2001','2000','1999','1995','1990'],
-                                                        var_name="Year",
-                                                        value_name="Rates").sort_values(by=["State","Year"])
-
-new_DF.to_csv('/Users/Evan/Desktop/new_HW1_data.csv')
+    divorce = divorce.replace('---', 'Null')
+    divorce.dropna(how='all', inplace=True)
+    divorce = divorce.stack([0]).reset_index()
+    # print (divorce)
+    divorce.rename(columns={divorce.columns[0]: 'State',
+                            divorce.columns[1]: 'Year',
+                            divorce.columns[2]: 'divorce_rate',
+                            }
+                   , inplace=True)
+    divorce.to_excel(excel_writer='clean_divorce.xlsx',  # naming the new excel file
+                     sheet_name='divorce_rate',  # name of the sheet
+                     na_rep='null',
+                     index=False)
